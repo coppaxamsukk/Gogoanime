c124acc174c2f5ab98405db13c5a119613c22866
coppaxamsukk <coppaxamsu@gmail.com>
2024-09-27 02:59:19 +0600

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,9 +1,9 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = ""; // add the api here with the '/' at the end!
-$consumet = ""; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
+$apiLink = "https://consumet-api-goep.onrender.com/anime/gogoanime/"; // add the api here with the '/' at the end!
+$consumet = "https://consumet-api-goep.onrender.com/anime/gogoanime/"; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
 if (date("d") > 15){
-    $apiLink = ""; // same goes for here
+    $apiLink = "https://consumet-api-goep.onrender.com/anime/gogoanime/"; // same goes for here
 }
 ?>
